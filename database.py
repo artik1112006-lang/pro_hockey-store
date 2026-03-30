13e32b35c0ef4fbe3fd87eb280be143528f0905b
artik1112006-lang <artik1112006@gmail.com>
2026-03-30 03:52:16 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,28 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+# НОВОЕ ИМЯ БАЗЫ, ЧТОБЫ ИЗБЕЖАТЬ ОШИБОК КОЛОНОК
+SQLALCHEMY_DATABASE_URL = "sqlite:///./hockey_final.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    products = relationship("Product", back_populates="category", cascade="all, delete")
+
 class Product(Base):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    price = Column(Float)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
     description = Column(String)
     image_url = Column(String)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="products")
 
 class Order(Base):
     __tablename__ = "orders"
